42166e166d27b2557b30ca65a30e77c3e3e59921
idrissahil <idrissahil3@gmail.com>
2019-05-18 19:18:41 +0200

    added rd launch file for wifi and control node

--- src/dd_control/src/control_decision_2.py
+++ src/dd_control/src/control_decision_2.py
@@ -18,17 +18,22 @@ def callback_gps(gps):
     global curr_pos
     global rrt_list
     global state
-    curr_pos[0]=gps.pose.position.x
-    curr_pos[1]=gps.pose.position.y
-    curr_pos[2]=gps.pose.position.z
+    if state==1:
+        curr_pos[0]=gps.pose.position.x
+        curr_pos[1]=gps.pose.position.y
+        curr_pos[2]=gps.pose.position.z
 
     if rrt_ori==1:
         state=2
         print(state)
         hold_position=PoseStamped()
-        hold_position.pose.position.x= 0
-        hold_position.pose.position.y = 14
-        hold_position.pose.position.z= 1
+        #hold_position.pose.position.x= 0
+        #hold_position.pose.position.y = 14
+        #hold_position.pose.position.z= 1
+
+        hold_position.pose.position.x= curr_pos[0]
+        hold_position.pose.position.y = curr_pos[1]
+        hold_position.pose.position.z= curr_pos[2]
         hold_position.header.frame_id = "map"
         control_decision_pub.publish(hold_position)
 
